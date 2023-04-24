69806406a88ac89ae7edc63039f032e6028ce42c
victorpraiz <ivictorpraiz@gmail.com>
2023-04-24 09:12:01 +0100

    Configured services to log to text file

--- WebsiteStatus/Program.cs
+++ WebsiteStatus/Program.cs
@@ -1,10 +1,33 @@
 using WebsiteStatus;
+using Serilog;
+using Serilog.Formatting.Compact;
 
-IHost host = Host.CreateDefaultBuilder(args)
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.File("worker-logs/log.txt", rollingInterval: RollingInterval.Hour)
+    .CreateLogger();
+
+try
+{
+    Log.Information("Starting up the service");
+    IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
-    })
+    }).UseSerilog()
     .Build();
 
-await host.RunAsync();
+    await host.RunAsync();
+    return;
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "There was a problem starting this service");
+    return;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
--- WebsiteStatus/Worker.cs
+++ WebsiteStatus/Worker.cs
@@ -3,10 +3,24 @@ namespace WebsiteStatus
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private HttpClient client;
+        private string webUrl = "https://www.victormadu.tech";
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            client = new HttpClient();
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            client.Dispose();
+            return base.StopAsync(cancellationToken);
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -14,7 +28,18 @@ namespace WebsiteStatus
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                var result = await client.GetAsync(webUrl);
+                _logger.LogInformation("Getting result completed");
+
+                if (result.IsSuccessStatusCode)
+                {
+                    _logger.LogInformation("Website is up and running at {statusCode}", result.StatusCode);
+                }
+                else
+                {
+                    _logger.LogError("Website is down. SOS! Status code: {statusCode}", result.StatusCode);
+                }
+                await Task.Delay(5000, stoppingToken);
             }
         }
     }
